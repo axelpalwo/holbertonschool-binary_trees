a5e382f0146dc8ee69f42edb6bb7d160e78c24c5
Swagtiago29 <sant29ramos@gmail.com>
2024-08-05 19:28:00 +0000

    fix height 4

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	const binary_tree_t *current;
 	size_t i = 0;
+	size_t o = 0;
 
 	current = tree;
 	if (!tree)
@@ -18,5 +19,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		i++;
 		current = current->left;
 	}
-	return (i);
+	current = tree;
+	while (current-right && current->left)
+	{
+		o++;
+		current = current->right;
+	}
+	if (o < i)
+		return (i);
+	return (o);
 }

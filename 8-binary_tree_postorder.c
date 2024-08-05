503fc4fe3077a74751804eaec8d1eff25ad07494
Axel <Axel.palombo.ap@gmail.com>
2024-08-05 19:03:37 +0000

    eight exercise

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Goes through a binary tree using post order traversal
+ * @tree: Pointer to tree
+ * @func: Function to print data
+ * Return: None
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}

e10f46140ec335abd5a89539799238fb0dcd7f21
Axel <Axel.palombo.ap@gmail.com>
2024-08-05 17:44:22 +0000

    fixed 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,9 +22,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent->left->parent = new_node;
 		new_node->left = parent->left;
-		parent->left = new_node;	
 	} else
 		new_node->left = NULL;
-
+	parent->left = new_node;	
 	return (new_node);
 }

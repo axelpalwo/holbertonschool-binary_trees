2e6e4bc0165f5e20b9bbc761f72b9f0255e3e2e5
Axel <Axel.palombo.ap@gmail.com>
2024-08-05 17:11:36 +0000

    betty fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -21,9 +21,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent == NULL)
-		return (new_node);
-	else
+	if (parent != NULL)
 	{
 		if (parent->left)
 			parent->right = new_node;

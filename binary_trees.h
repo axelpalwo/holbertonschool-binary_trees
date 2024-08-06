107ba1f20bc811883399f434fafb21fd154bf26e
Axel <Axel.palombo.ap@gmail.com>
2024-08-06 15:13:20 +0000

    fourteen exercise

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t leaves(const binary_tree_t *tree,size_t total);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */

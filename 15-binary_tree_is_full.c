372fb33809ea4ed51175506149d7f2e5863157e4
Axel <Axel.palombo.ap@gmail.com>
2024-08-06 17:50:15 +0000

    trying to fix 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,6 +2,7 @@
 /**
  * getfulltree - Get if a binary tree is full
  * @tree: Pointer to node in tree
+ * @result: Result of function
  * Return: 0 Success / 1 Failure
  */
 int getfulltree(const binary_tree_t *tree, int result)
@@ -12,7 +13,8 @@ int getfulltree(const binary_tree_t *tree, int result)
 			result *= getfulltree(tree->left, result);
 		if (tree->right)
 			result *= getfulltree(tree->right, result);
-		if ((!tree->left && !tree->right) || (tree->left && tree->left))
+		if ((!tree->left && !tree->right) 
+				|| (tree->left && tree->left))
 			return (result * 1);
 	}
 	return (result * 0);
@@ -28,6 +30,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (getfulltree(tree, 1) == 1)
 		return (1);
-
 	return (0);
 }

9e3e3749516f5c93c5b82d7bca91eb2e0d8b9ff4
Axel <Axel.palombo.ap@gmail.com>
2024-08-06 13:47:42 +0000

    eleventh exercise

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+/**
+ * getsize - gets size
+ * @tree: node
+ * @count: Count
+ * Return: Size Count
+ */
+size_t getsize(const binary_tree_t *tree, size_t count)
+{
+	size_t a = count, b = count;
+	if (tree)
+	{
+		if (tree->left)
+			a += getsize(tree->left, count);
+		if (tree->right)
+			b += getsize(tree->right, count);
+	}
+//	printf("Donde el nodo vale %u, count es %li\n", tree->n, count);
+	return ((a + b));
+}
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to root node
+ * Return: Size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (getsize(tree, 1) / 2);
+}

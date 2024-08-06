db3e6e87be282d57d03682fda8814e5944ea1ef7
Axel <Axel.palombo.ap@gmail.com>
2024-08-06 17:35:11 +0000

    fifteen exercise

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * getfulltree - Get if a binary tree is full
+ * @tree: Pointer to node in tree
+ * Return: 0 Success / 1 Failure
+ */
+int getfulltree(const binary_tree_t *tree)
+{
+	int result = 1;
+
+	if (tree)
+	{
+		if (tree->left)
+			result = getfulltree(tree->left);
+		if (tree->right)
+			result = getfulltree(tree->right);
+		if ((!tree->left && !tree->right) || (tree->left && tree->left))
+			return (result * 1);
+	}
+	return (result * 0);
+}
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to tree
+ * Return: 1 Success / 0 Failure
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (getfulltree(tree) == 1)
+		return (1);
+
+	return (0);
+}
